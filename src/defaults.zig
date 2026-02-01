f9052b87211395a197b234881a9a506051af4939
Léon van der Kaap <leonkaap@gmail.com>
2026-02-01 14:32:20 +0100

    Fixed linebreak issue

--- src/defaults.zig
+++ src/defaults.zig
@@ -1,2 +1,2 @@
-const default_source_file_location: []const u8 = "";
-const default_destination_file_location: []const u8 = "";
+pub const default_source_file_location: []const u8 = "";
+pub const default_destination_file_location: []const u8 = "";
